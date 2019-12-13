5e234e88537ebbc4cd6417da0d6c8dd1c0554828
davidpm <davidpmartin@gmail.com>
2019-12-13 13:58:36 -0500

    set to do, create course preferences, small changes to parameters

--- src/CoursePreferences.java
+++ src/CoursePreferences.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 //use a hashtable store course id/weight pair values.  use id to search hash table then rank stored values to generate preferences
 public class CoursePreferences {
     Scanner keyboard = new Scanner(System.in);
+    System.out.println("State the course you wish to enter")
     String preference = keyboard.nextLine();
-    int weight;
+    do {
+        int weight = keyboard.nextInt();
+        if(weight < 1) {
+            System.out.println("Your answer is invalid");
+        }
+    } while (weight < = 0);
+
 }
